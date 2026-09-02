6e2efe24fec0b6be9abf4a3f89ea8b1aaa545898
Tsiky Ny Antsa <randriamanantenatsikynyantsa@gmail.com>
2026-09-02 23:35:48 +0300

    fix(ssr): keep server environment browser-free

--- artifacts/jerymotro/src/entry-server.tsx
+++ artifacts/jerymotro/src/entry-server.tsx
@@ -1,33 +1,3 @@
-// Mock window and document before importing React code so Leaflet/browser-only modules don't crash Node on import.
-if (typeof global !== "undefined") {
-  const domMock = {
-    location: {
-      pathname: "/",
-      protocol: "https:",
-      hostname: "jerymotro.duckdns.org",
-    },
-    addEventListener: () => {},
-    removeEventListener: () => {},
-    dispatchEvent: () => {},
-  };
-  (global as any).window = domMock;
-  (global as any).document = {
-    createElement: () => ({ style: {} }),
-    documentElement: { style: {} },
-  };
-  Object.defineProperty(global, "navigator", {
-    value: { userAgent: "node" },
-    writable: true,
-    configurable: true,
-  });
-  (global as any).localStorage = {
-    getItem: () => null,
-    setItem: () => {},
-    removeItem: () => {},
-    clear: () => {},
-  };
-}
-
 import ReactDOMServer from "react-dom/server";
 import App from "./App";
 
